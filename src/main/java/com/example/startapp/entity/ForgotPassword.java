a74c77c37e2974e124a74f4807138cc8ded48134
Aytac <aramazanli@std.beu.edu.az>
2024-07-21 01:56:57 +0400

    added ForgotPassword

--- src/main/java/com/example/startapp/entity/ForgotPassword.java
+++ src/main/java/com/example/startapp/entity/ForgotPassword.java
@@ -1,10 +1,7 @@
 package com.example.startapp.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Date;
 
@@ -12,6 +9,7 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 @Builder
 public class ForgotPassword {
 
