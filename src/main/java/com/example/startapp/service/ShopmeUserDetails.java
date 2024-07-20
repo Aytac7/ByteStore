8e2d9aa03cce4f9f719a3955db7a4b147113c6d5
Aytac <aramazanli@std.beu.edu.az>
2024-07-21 02:03:16 +0400

    added ShopmeUserDetails

---
+++ src/main/java/com/example/startapp/service/ShopmeUserDetails.java
@@ -0,0 +1,81 @@
+package com.example.startapp.service;
+
+import com.example.startapp.entity.User;
+import com.example.startapp.enums.UserRole;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.management.relation.Role;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+
+@Getter
+public class ShopmeUserDetails implements UserDetails {
+    private  User user;
+     
+    public ShopmeUserDetails(User user) {
+        this.user = user;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UserRole role = user.getRole();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(role.name()));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+
+    public String getFullName(){
+        return user.getName()+" " + user.getSurname();
+    }
+
+    public void setName(String name){
+        this.user.setName(name);
+    }
+
+    public void setSurname(String surname){
+        this.user.setName(surname);
+    }
+
+    public User getUser(){
+        return this.user;
+    }
+
+    // other overridden methods...
+}
