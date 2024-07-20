2939a364c980214367bfa94e94f114fa9b6dff63
Aytac <aramazanli@std.beu.edu.az>
2024-07-21 02:00:30 +0400

    added LoginRequest

--- src/main/java/com/example/startapp/dto/request/LoginRequest.java
+++ src/main/java/com/example/startapp/dto/request/LoginRequest.java
@@ -16,5 +16,7 @@ public class LoginRequest {
     @NotBlank(message = "Email cannot be blank")
     @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "The password field can't be blank")
     private String password;
 }
