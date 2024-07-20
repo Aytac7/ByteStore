54724628e6c08d0e7ea702b87b243c6e33c4ae00
Aytac <aramazanli@std.beu.edu.az>
2024-07-21 02:02:30 +0400

    added SecurityConfiguration

--- src/main/java/com/example/startapp/config/SecurityConfiguration.java
+++ src/main/java/com/example/startapp/config/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package com.example.startapp.config;
 
 import com.example.startapp.service.AuthFilterService;
+import com.example.startapp.service.CustomLoginFailureHandler;
+import com.example.startapp.service.CustomLoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,14 +20,19 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 @EnableWebSecurity
 @EnableMethodSecurity
-public class SecurityConfiguration {
+public class SecurityConfiguration  {
 
     private final AuthFilterService authFilterService;
     private final AuthenticationProvider authenticationProvider;
 
+    private final CustomLoginFailureHandler customLoginFailureHandler;
+    private final CustomLoginSuccessHandler customLoginSuccessHandler;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+
+
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**", "/forgotPassword/**")
@@ -34,8 +42,8 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
+                .formLogin(AbstractHttpConfigurer::disable) // Disable form login
                 .addFilterBefore(authFilterService, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
-    }
-}
+    }}
