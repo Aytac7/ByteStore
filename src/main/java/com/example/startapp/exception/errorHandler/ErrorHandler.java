7983e8f0e5e0cbd77ec84078dbdeabf4e1f2a915
Aytac <aramazanli@std.beu.edu.az>
2024-07-21 01:56:23 +0400

    added ErrorHandler

--- src/main/java/com/example/startapp/exception/errorHandler/ErrorHandler.java
+++ src/main/java/com/example/startapp/exception/errorHandler/ErrorHandler.java
@@ -1,15 +1,19 @@
 package com.example.startapp.exception.errorHandler;
 
-import com.example.startapp.exception.PasswordInvalidException;
-import com.example.startapp.exception.UserEmailExistsException;
-import com.example.startapp.exception.UserNotFoundException;
+import com.example.startapp.exception.*;
 
 import com.example.startapp.exception.error.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -49,4 +53,59 @@ public class ErrorHandler {
                 .build();
     }
 
+
+    @ExceptionHandler(CustomLockedException.class)
+    @ResponseStatus(HttpStatus.LOCKED)
+    public ErrorResponse handleLockedException(CustomLockedException exception) {
+        log.error("handleLockedException {}", exception.getMessage());
+
+        return ErrorResponse.builder()
+                .code(HttpStatus.LOCKED.name())
+                .message(exception.getMessage())
+                .build();
+    }
+    @ExceptionHandler(RefreshTokenNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handlerRefreshTokenNotFoundException(Exception exception) {
+        log.error("handlerRefreshTokenNotFoundException {}", exception.getMessage());
+
+        return ErrorResponse.builder()
+                .code(HttpStatus.NOT_FOUND.name())
+                .message(exception.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(RefreshTokenExpiredException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponse handlerRefreshTokenExpiredException(Exception exception) {
+        log.error("handlerRefreshTokenExpiredException {}", exception.getMessage());
+
+        return ErrorResponse.builder()
+                .code(HttpStatus.UNAUTHORIZED.name())
+                .message(exception.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(InvalidOtpException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handlerInvalidOtpException(Exception exception) {
+        log.error("handlerInvalidOtpException {}", exception.getMessage());
+
+        return ErrorResponse.builder()
+                .code(HttpStatus.BAD_REQUEST.name())
+                .message(exception.getMessage())
+                .build();
+    }
+
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+//        Map<String, String> errors = new HashMap<>();
+//        ex.getBindingResult().getAllErrors().forEach(error -> {
+//            String fieldName = ((FieldError) error).getField();
+//            String errorMessage = error.getDefaultMessage();
+//            errors.put(fieldName, errorMessage);
+//        });
+//        return errors;
+//    }
 }
