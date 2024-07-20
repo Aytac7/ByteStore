d2c602688a70a91bb95c53c36695faa09878ef05
Aytac <aramazanli@std.beu.edu.az>
2024-07-21 01:53:45 +0400

    added CustomLockedException

---
+++ src/main/java/com/example/startapp/exception/CustomLockedException.java
@@ -0,0 +1,9 @@
+package com.example.startapp.exception;
+
+public class CustomLockedException extends RuntimeException {
+    public CustomLockedException(String message) {
+        super(message);
+    }
+
+
+}
