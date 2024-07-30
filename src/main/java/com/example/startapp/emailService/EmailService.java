3661ad926d434078dfd43bfb0b6471b98ff48ffc
Nazrin Asgarli <nazrin.shikhalievaa@gmail.com>
2024-07-30 16:37:34 +0400

    liquibase, phoneprefix added

--- src/main/java/com/example/startapp/emailService/EmailService.java
+++ src/main/java/com/example/startapp/emailService/EmailService.java
@@ -6,24 +6,25 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.Properties;
+
 @Service
 
 public class EmailService {
     private final JavaMailSender javaMailSender;
 
-    public EmailService(JavaMailSender javaMailSender){
-        this.javaMailSender=javaMailSender;
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
     }
 
-    public void sendSimpleMessage(MailBody mailBody){
-        SimpleMailMessage message=new SimpleMailMessage();
+    public void sendSimpleMessage(MailBody mailBody) {
+        SimpleMailMessage message = new SimpleMailMessage();
 
         message.setTo(mailBody.to());
-        message.setFrom("aytacr67@gmail.com");
+        message.setFrom("nazrin.shikhalievaa@gmail.com");
         message.setSubject(mailBody.subject());
         message.setText(mailBody.text());
 
         javaMailSender.send(message);
     }
-
 }
