bb33ab56bb5acd303127ca6e4ed6be31fdbcaf03
Aytac <aramazanli@std.beu.edu.az>
2024-08-09 17:57:39 +0400

    added subcategory entity, added updated and created field to ad,  favorite ,image

--- src/main/java/com/example/startapp/entity/Favorite.java
+++ src/main/java/com/example/startapp/entity/Favorite.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "favorites")
@@ -25,4 +29,10 @@ public class Favorite {
     @ManyToOne
     @JoinColumn
     private Ad ad;
+
+    @CreationTimestamp
+    LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    LocalDateTime updatedAt;
 }
