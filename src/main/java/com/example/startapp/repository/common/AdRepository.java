b9efebc73cce7be8fe69bde4a3679e01d68c947b
Aytac <aramazanli@std.beu.edu.az>
2024-08-29 22:15:43 +0400

    filter-ads

--- src/main/java/com/example/startapp/repository/common/AdRepository.java
+++ src/main/java/com/example/startapp/repository/common/AdRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.example.startapp.dto.response.common.AdDTO;
 
@@ -19,6 +20,9 @@ import java.util.Optional;
 @Repository
 public interface AdRepository extends JpaRepository<Ad, Long>,
         PagingAndSortingRepository<Ad, Long>, JpaSpecificationExecutor<Ad> {
+
+    @Query("SELECT a FROM Ad a WHERE (LOWER(a.brand.name) LIKE LOWER(CONCAT('%', :searchQuery, '%')) OR LOWER(a.model.name) LIKE LOWER(CONCAT('%', :searchQuery, '%'))) AND a.status = 'APPROVED'")
+    Page<Ad> findSuggestions(@Param("searchQuery") String searchQuery, Pageable pageable);
 
 
     List<Ad> findAllByStatus(AdStatus status);
--- src/main/java/com/example/startapp/service/specification/AdSpecification.java
+++ src/main/java/com/example/startapp/service/specification/AdSpecification.java
@@ -2,6 +2,8 @@ package com.example.startapp.service.specification;
 
 import com.example.startapp.dto.request.common.AdCriteriaRequest;
 import com.example.startapp.entity.Ad;
+import com.example.startapp.entity.Brand;
+import com.example.startapp.entity.Model;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
@@ -17,21 +19,66 @@ public class AdSpecification implements Specification<Ad> {
         return(root, query, criteriaBuilder) -> {
             List<Predicate>predicates=new ArrayList<>();
 
-            if (adCriteriaRequest.getStatus() != null) {
-                predicates.add(criteriaBuilder.equal(root.get("status"), adCriteriaRequest.getStatus()));
+            predicates.add(criteriaBuilder.equal(root.get("status"), "APPROVED"));
+
+
+            if (adCriteriaRequest.getPriceFrom() > 0 && adCriteriaRequest.getPriceTo() > 0) {
+                predicates.add(criteriaBuilder.between(root.get("price"),
+                        adCriteriaRequest.getPriceFrom(), adCriteriaRequest.getPriceTo()));
+            }
+//            else if (adCriteriaRequest.getPriceFrom() > 0) {
+//                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("price"), adCriteriaRequest.getPriceFrom()));
+//            } else if (adCriteriaRequest.getPriceTo() > 0) {
+//                predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("price"), adCriteriaRequest.getPriceTo()));
+//            }
+
+
+            if (adCriteriaRequest.getBrandIds() != null && !adCriteriaRequest.getBrandIds().isEmpty()) {
+                predicates.add(root.get("brand").get("id").in(adCriteriaRequest.getBrandIds()));
+            }
+
+            if (adCriteriaRequest.getModelIds() != null && !adCriteriaRequest.getModelIds().isEmpty()) {
+                predicates.add(root.get("model").get("id").in(adCriteriaRequest.getModelIds()));
             }
 
 
-            if (adCriteriaRequest.getHeader() != null && !adCriteriaRequest.getHeader().isEmpty()) {
-                predicates.add(criteriaBuilder.like(
-                        criteriaBuilder.lower(root.get("header")),
-                        "%" + adCriteriaRequest.getHeader().toLowerCase() + "%"
-                ));
+
+            if (adCriteriaRequest.getIsNew()!=null ) {
+                if (adCriteriaRequest.getIsNew()) {
+                    predicates.add(criteriaBuilder.isTrue(root.get("isNew")));
+
+                } else {
+                    predicates.add(criteriaBuilder.isFalse(root.get("isNew")));
+                }
             }
+
+
+
+
+            if (adCriteriaRequest.isSortByNewest()) {
+                query.orderBy(criteriaBuilder.desc(root.get("createdAt")));
+            }
+
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
 
         };
+
     }
+
+//    public static Specification<Ad> searchByBrandOrModel(String searchQuery) {
+//        return (Root<Ad> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) -> {
+//            if (searchQuery == null || searchQuery.isEmpty()) {
+//                return criteriaBuilder.conjunction();
+//            }
+//
+//            String searchPattern = "%" + searchQuery.toLowerCase() + "%";
+//
+//            Predicate brandPredicate = criteriaBuilder.like(criteriaBuilder.lower(root.get("brand").get("name")), searchPattern);
+//            Predicate modelPredicate = criteriaBuilder.like(criteriaBuilder.lower(root.get("model").get("name")), searchPattern);
+//
+//            return criteriaBuilder.or(brandPredicate, modelPredicate);
+//        };
+//    }
     @Override
     public Predicate toPredicate(Root<Ad> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
         return null;
