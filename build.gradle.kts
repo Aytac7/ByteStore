e12beb274b3315a44d3890b561966ba1b6664a66
Nazrin Asgarli <nazrin.shikhalievaa@gmail.com>
2024-09-12 17:21:37 +0400

    user management

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,22 +26,22 @@ dependencies {
 
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation ("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
 
     implementation("org.springframework.boot:spring-boot-starter-web-services")
     implementation("org.springframework.boot:spring-boot-starter-mail")
     runtimeOnly("org.postgresql:postgresql")
-    implementation ("org.mapstruct:mapstruct:1.5.5.Final")
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
-    implementation ("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("com.h2database:h2")
 
-    implementation ("io.jsonwebtoken:jjwt-api:0.11.5")
-    runtimeOnly ("io.jsonwebtoken:jjwt-impl:0.11.5")
-    runtimeOnly ("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
     implementation("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
@@ -59,20 +59,15 @@ dependencies {
 //    implementation("com.amazonaws:aws-java-sdk-s3:1.12.767")
 
 
-   // implementation("io.swagger:swagger-annotations:1.6.4")
+    // implementation("io.swagger:swagger-annotations:1.6.4")
     implementation("org.springdoc:springdoc-openapi-ui:1.7.0")
 
+    implementation("io.swagger:swagger-annotations:1.6.4")
 
-//    implementation("jakarta.validation:jakarta.validation-api:3.0.2")
     implementation("jakarta.validation:jakarta.validation-api:3.0.2")
     implementation("org.springframework.boot:spring-boot-starter-validation:3.3.2")
     implementation("org.hibernate.validator:hibernate-validator:8.0.1.Final")
-
-
-
-
 }
-
 tasks.withType<Test> {
     useJUnitPlatform()
 }
--- src/main/java/com/example/startapp/service/auth/UserService.java
+++ src/main/java/com/example/startapp/service/auth/UserService.java
@@ -1,16 +1,90 @@
 package com.example.startapp.service.auth;
 
+import com.example.startapp.dto.request.UserInfoRequest;
+import com.example.startapp.entity.common.Image;
+import com.example.startapp.entity.auth.User;
 import com.example.startapp.repository.auth.UserRepository;
+import com.example.startapp.repository.common.ImageRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final S3Service s3Service;
+    private final ImageRepository imageRepository;
 
 
+    @Transactional
+    public void updateInfo(Long userId, UserInfoRequest userInfoRequest, MultipartFile file) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("İstifadəçi tapılmadı id: " + userId));
+
+        user.setName(userInfoRequest.getName());
+        user.setSurname(userInfoRequest.getSurname());
+        user.setPhonePrefix(userInfoRequest.getPhonePrefix());
+        user.setPhoneNumber(userInfoRequest.getPhoneNumber());
+
+        if (file != null && !file.isEmpty()) {
+            String key = "profilephoto/" + userId + "/" + file.getOriginalFilename();
+            String fileUrl;
+            try {
+                fileUrl = s3Service.uploadFile(key, file);
+            } catch (IOException e) {
+                throw new RuntimeException("Yeni profil şəkli yükləyəndə xəta yarandı. " + e.getMessage());
+            }
+
+            if (user.getProfilePhoto() != null) {
+                Image existingProfilePhoto = user.getProfilePhoto();
+                existingProfilePhoto.setFileName(file.getOriginalFilename());
+                existingProfilePhoto.setFileType(file.getContentType());
+                existingProfilePhoto.setFilePath(fileUrl);
+                existingProfilePhoto.setImageUrl(fileUrl);
+
+                imageRepository.save(existingProfilePhoto);
+                System.out.println("Profil şəkli yükləndi");
+
+            } else {
+                Image newProfilePhoto = new Image();
+                newProfilePhoto.setFileName(file.getOriginalFilename());
+                newProfilePhoto.setFileType(file.getContentType());
+                newProfilePhoto.setFilePath(fileUrl);
+                newProfilePhoto.setImageUrl(fileUrl);
+
+                newProfilePhoto.setUser(user);
+
+                Image savedProfilePhoto = imageRepository.save(newProfilePhoto);
+                System.out.println("Yeni profil şəkli saxlanıldı id: " + savedProfilePhoto.getId());
+                user.setProfilePhoto(savedProfilePhoto);
+            }
+
+        } else {
+            System.out.println("Heç bir fayl yüklənmədi");
+        }
+        userRepository.save(user);
+        System.out.println("İstifadəçi məlumatları yeniləndi");
+    }
 
 
+    @Transactional
+    public void deleteProfilePhoto(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("İstifadəçi tapılmadı id: " + userId));
 
+        if (user.getProfilePhoto() != null) {
+            Image profilePhoto = user.getProfilePhoto();
+            String imageKey = profilePhoto.getFilePath();
+            s3Service.deleteFile(imageKey);
+            imageRepository.delete(profilePhoto);
+            user.setProfilePhoto(null);
+            userRepository.save(user);
+
+
+        } else {
+            throw new RuntimeException("Profil şəkli yoxdu");
+        }
+    }
 }
